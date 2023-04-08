d57ea910cd1640840d1df7eae12883539ceeb11e
EmoryK <105027715+EmoryK@users.noreply.github.com>
2023-04-07 21:28:28 -0500

    Basic Achievement class

---
+++ code/Achievement.java
@@ -0,0 +1,29 @@
+package code;
+
+public class Achievement {
+    private String name;
+    private String description;
+    private boolean completed;
+
+    public Achievement(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
