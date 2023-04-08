4981d8856d7453dec705935826292838c29f7c07
EmoryK <105027715+EmoryK@users.noreply.github.com>
2023-04-07 22:32:32 -0500

    added more getters and tracker variables

--- code/Achievement.java
+++ code/Achievement.java
@@ -4,16 +4,30 @@ public class Achievement {
     private String name;
     private String description;
     private boolean completed;
+    private int extraCoins;
+    private int requiredChores;
 
-    public Achievement(String name, String description) {
+    public Achievement(String name, String description, int extraCoins, int requiredChores) {
         this.name = name;
         this.description = description;
+        this.extraCoins = extraCoins;
+        this.requiredChores = requiredChores;
         this.completed = false;
     }
 
+
+    public int getExtraCoins() {
+        return extraCoins;
+    }
+    
+    public int getRequiredChores() {
+        return requiredChores;
+    }
+    
     public String getName() {
         return name;
     }
+
 
     public String getDescription() {
         return description;
