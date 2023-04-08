4cd64af7eb9a3f5f75d30856777f5fae1855691b
EmoryK <105027715+EmoryK@users.noreply.github.com>
2023-04-08 18:05:23 -0500

    Refactored
    In the refactored code, I broke down the SignUpFrame constructor into smaller methods that create individual components and panels. This makes the code more modular and easier to understand. The createLabel, createButton, and createPanel methods are responsible for creating labels, buttons, and panels, respectively. The createComponents method is responsible for putting everything together and adding the action listener to the sign-up button. Finally, the configureFrame method sets the frame properties.

--- code/SignUpFrame.java
+++ code/SignUpFrame.java
@@ -1,4 +1,5 @@
 package code;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -6,30 +7,61 @@ import java.util.*;
 import code.Parent;
 import java.lang.String;
 
-
 public class SignUpFrame extends JFrame {
     private JTextField usernameField;
     private JTextField passwordField;
 
     public SignUpFrame(ArrayList<Parent> parents) {
         super("Sign Up");
+        createComponents(parents);
+        configureFrame();
+    }
 
+    private void createComponents(ArrayList<Parent> parents) {
         // Create components
-        JLabel usernameLabel = new JLabel("Username:");
+        JLabel usernameLabel = createLabel("Username:");
         usernameField = new JTextField(20);
-        JLabel passwordLabel = new JLabel("Password:");
+        JLabel passwordLabel = createLabel("Password:");
         passwordField = new JTextField(20);
-        JButton signUpButton = new JButton("Sign Up");
+        JButton signUpButton = createButton("Sign Up");
 
-        // Customize components
-        Font playfulFont = new Font("Comic Sans MS", Font.PLAIN, 14);
-        usernameLabel.setFont(playfulFont);
-        passwordLabel.setFont(playfulFont);
-        signUpButton.setFont(playfulFont);
-        signUpButton.setBackground(new Color(255, 153, 102));
-        signUpButton.setForeground(Color.WHITE);
+        // Add action listener to sign up button
+        signUpButton.addActionListener(e -> {
+            Parent newParent = new Parent(UUID.randomUUID(), usernameField.getText(), passwordField.getText());
+            parents.add(newParent);
+
+            // Save new user's credentials
+            // (You should use a proper method to store user credentials, e.g., hashing passwords, and saving them in a database)
+            System.out.println("New user created: " + newParent.getName() + ", " + newParent.getPassword());
+
+            setVisible(false);
+            dispose();
+        });
 
         // Layout components using GridBagLayout
+        JPanel panel = createPanel(usernameLabel, usernameField, passwordLabel, passwordField, signUpButton);
+
+        // Add panel to frame
+        add(panel);
+    }
+
+    private JLabel createLabel(String labelText) {
+        JLabel label = new JLabel(labelText);
+        Font playfulFont = new Font("Comic Sans MS", Font.PLAIN, 14);
+        label.setFont(playfulFont);
+        return label;
+    }
+
+    private JButton createButton(String buttonText) {
+        JButton button = new JButton(buttonText);
+        Font playfulFont = new Font("Comic Sans MS", Font.PLAIN, 14);
+        button.setFont(playfulFont);
+        button.setBackground(new Color(255, 153, 102));
+        button.setForeground(Color.WHITE);
+        return button;
+    }
+
+    private JPanel createPanel(JLabel usernameLabel, JTextField usernameField, JLabel passwordLabel, JTextField passwordField, JButton signUpButton) {
         JPanel panel = new JPanel(new GridBagLayout());
         panel.setBackground(new Color(255, 255, 153));
         GridBagConstraints constraints = new GridBagConstraints();
@@ -53,26 +85,10 @@ public class SignUpFrame extends JFrame {
         constraints.gridy = 2;
         panel.add(signUpButton, constraints);
 
-        // Add action listener to sign up button
-        signUpButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Parent newParent = new Parent(UUID.randomUUID(), usernameField.getText(), passwordField.getText());
-                parents.add(newParent);
+        return panel;
+    }
 
-                // Save new user's credentials
-                // (You should use a proper method to store user credentials, e.g., hashing passwords, and saving them in a database)
-                System.out.println("New user created: " + newParent.getName() + ", " + newParent.getPassword());
-
-                setVisible(false);
-                dispose();
-            }
-        });
-
-        // Add panel to frame
-        add(panel);
-
-        // Set frame properties
+    private void configureFrame() {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
