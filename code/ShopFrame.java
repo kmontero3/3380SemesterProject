41575ecdd60e20a70300195bf0f376cf5eeb8a42
EmoryK <105027715+EmoryK@users.noreply.github.com>
2023-04-07 19:34:04 -0500

    Update ShopFrame.java

--- code/ShopFrame.java
+++ code/ShopFrame.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 
 class ShopFrame extends JFrame {
     private JLabel coinsLabel;
+
     public ShopFrame(Child child) {
         super("Shop");
 
@@ -16,11 +17,10 @@ class ShopFrame extends JFrame {
         coinsLabel = new JLabel("Coins: " + child.getCoinsAvailable());
         coinsLabel.setFont(playfulFont);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.setBackground(new Color(255, 255, 153));
-        panel.add(label);
-        panel.add(coinsLabel);
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setBackground(new Color(255, 255, 153));
+        headerPanel.add(label, BorderLayout.WEST);
+        headerPanel.add(coinsLabel, BorderLayout.EAST);
 
         // ActionListener for item buttons
         ActionListener itemButtonListener = new ActionListener() {
@@ -34,16 +34,18 @@ class ShopFrame extends JFrame {
 
         // Create shop items
         Font itemFont = new Font("Comic Sans MS", Font.PLAIN, 18);
-        
+        JPanel itemsPanel = new JPanel(new GridLayout(0, 3, 10, 10));
+        itemsPanel.setBackground(new Color(255, 255, 153));
+        itemsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
         for (Reward reward : Main.rewards) {
-            panel.add(createShopItem(itemFont, reward, "item1.png", itemButtonListener));
+            itemsPanel.add(createShopItem(itemFont, reward, "item1.png", itemButtonListener));
         }
 
         JButton backButton = new JButton("Back");
         backButton.setFont(playfulFont);
         backButton.setBackground(new Color(255, 153, 102));
         backButton.setForeground(Color.WHITE);
-        backButton.setPreferredSize(new Dimension(70, 30));
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,10 +53,16 @@ class ShopFrame extends JFrame {
                 dispose();
             }
         });
-        panel.add(backButton);
 
-        // Add panel to frame
-        add(panel);
+        JPanel footerPanel = new JPanel(new BorderLayout());
+        footerPanel.setBackground(new Color(255, 255, 153));
+        footerPanel.add(backButton, BorderLayout.WEST);
+
+        // Add panels to frame
+        setLayout(new BorderLayout());
+        add(headerPanel, BorderLayout.NORTH);
+        add(itemsPanel, BorderLayout.CENTER);
+        add(footerPanel, BorderLayout.SOUTH);
 
         // Set frame properties
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -65,7 +73,7 @@ class ShopFrame extends JFrame {
     }
 
     private JPanel createShopItem(Font itemFont, Reward reward, String itemImagePath, ActionListener itemButtonListener) {
-        JPanel itemPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel itemPanel = new JPanel(new BorderLayout(10, 10));
         itemPanel.setBackground(new Color(255, 255, 153));
         itemPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 
@@ -77,12 +85,11 @@ class ShopFrame extends JFrame {
         itemButton.addActionListener(itemButtonListener);
         itemButton.putClientProperty("reward", reward);
 
-        itemPanel.add(itemButton);
-        itemPanel.add(itemLabel);
+        itemPanel.add(itemButton, BorderLayout.CENTER);
+        itemPanel.add(itemLabel, BorderLayout.SOUTH);
 
         return itemPanel;
     }
-
     private void handlePurchase(Child child, Reward reward) {
         if (child.getCoinsAvailable() >= reward.getCoinAmount()) {
             System.out.println(child.getName() + " has successfully purchased " + reward.getName() + "!");
